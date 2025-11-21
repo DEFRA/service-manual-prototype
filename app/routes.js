b463edc7141c05657ee3e7b75a2c9b19ee5e5e56
Chris Leo <cwmleo@gmail.com>
2025-11-21 17:03:47 +0000

    Migrating prototype content

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,27 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+// Manual routes
+router.get('/', function (req, res) {
+  res.render('manual/index')
+})
+
+router.get('/service-standard', function (req, res) {
+  res.render('manual/service-standard')
+})
+
+router.get('/components', function (req, res) {
+  res.render('manual/components')
+})
+
+router.get('/patterns', function (req, res) {
+  res.render('manual/patterns')
+})
+
+router.get('/accessibility', function (req, res) {
+  res.render('manual/accessibility')
+})
+
+router.get('/working-with-defra', function (req, res) {
+  res.render('manual/working-with-defra')
+})
